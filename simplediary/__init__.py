80d4ce5f81dbca90091f9b4c0fa49948beca4e45
岡崎 祥明 <okazaki@minimal-hitech.com>
2016-09-29 15:44:42 +0900

    アプリの設定の記述と起動まで

--- simplediary/__init__.py
+++ simplediary/__init__.py
@@ -1,10 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello World!!"
-
-if __name__ == '__main__':
-    app.run()
+app.config.from_object('simplediary.config')
+app.config.from_pyfile('config.py')
