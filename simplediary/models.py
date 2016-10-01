27b1fbe7ae0fd88105454ff0c70eecd86e4c29a7
岡崎 祥明 <okazaki@minimal-hitech.com>
2016-10-01 14:37:29 +0900

    DBの設計を変更

--- simplediary/models.py
+++ simplediary/models.py
@@ -1,17 +1,42 @@
 from simplediary import db
 
-class Diary(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    stock_number = db.Column(db.Integer)
-    feeding = db.Column(db.String(140))
-    report = db.Column(db.Text)
-    user = db.relationship("User", backref = db.backref('Diary'))
-    date = db.Column(db.Date, nullable=False)
-    update = db.Column(db.DateTime, nullable=False)
-    created = db.Column(db.DateTime, nullable=False)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True)
     _password = db.Column('password', db.String(140), nullable=False)
+    diary = db.relationship("Diary", backref=db.backref('User'))
+
+class Pond(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pondname = db.Column(db.String(140), nullable=False)
+    diary = db.relationship("Diary", backref=db.backref('Pond'))
+
+class Season(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    seasonname = db.Column(db.String(140), nullable=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date)
+    note = db.Column(db.Text)
+    diary = db.relationship("Diary", backref=db.backref('Season'))
+
+class Diary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pond_id = db.Column(db.Integer, db.ForeignKey('pond.id'), nullable=False)
+    season_id = db.Column(db.Integer, db.ForeignKey('season.id'), nullable=False)
+    stock_number = db.Column(db.Integer)
+    feeding = db.Column(db.String(140))
+    report = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    update = db.Column(db.DateTime, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    pond = db.relationship("Pond", backref=db.backref('Diary'))
+    season = db.relationship("Season", backref=db.backref('Diary'))
+    user = db.relationship("User", backref=db.backref('Diary'))
+
+
+    # @hybrid_property
+    # def password(self):
+    #     return self._password
