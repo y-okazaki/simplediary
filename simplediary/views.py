76dfce7681698e892bdd7b99cf2372d116311787
岡崎 祥明 <okazaki@minimal-hitech.com>
2016-10-08 15:15:34 +0900

    ユーザー一覧ページを追加

--- simplediary/views.py
+++ simplediary/views.py
@@ -1,12 +1,18 @@
 from flask import render_template, request, redirect, url_for, flash
 from simplediary import app, db
 from simplediary.models import User, Pond, Season, Diary
-from simplediary.forms import UserForm
+from simplediary.forms import UserForm, UserEditForm
 
 @app.route('/')
 def index():
     data = Diary.query.order_by('id desc')
     return render_template('index.html', diaries=data)
+
+@app.route('/user/', methods=['GET', 'POST'])
+def user_list():
+    user_list = User.query.order_by('id desc')
+    return render_template('user/list.html', user_list=user_list)
+
 
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
@@ -19,3 +25,13 @@ def add_user():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('user/add.html', form=form)
+
+# @app.route('/user/<user_id>/edit', methods=['GET', 'POST'])
+# def edit_user(user_id):
+#     user = User.query.filter_by(id=user_id).first_or_404()
+#     form = UserEditForm(request.form, user)
+#     if form.validate_on_submit():
+#         form.populate_obj(user)
+#         db.session.commit()
+#         return redirect(url_for('index'))
+#     return render_template('user/edit.html', form=form)
