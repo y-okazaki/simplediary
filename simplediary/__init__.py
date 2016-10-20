9190f2a45f1915c2757080b49e9595984da85fe9
岡崎 祥明 <okazaki@minimal-hitech.com>
2016-10-20 11:06:48 +0900

    config.pyに関するコメントを追加

--- simplediary/__init__.py
+++ simplediary/__init__.py
@@ -8,6 +8,7 @@ csrf = CsrfProtect()
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('simplediary.config')
+# 本番環境の設定に上書き（ルートにinstanceファルダを作りその中にconfig.pyを置く）
 app.config.from_pyfile('config.py')
 CsrfProtect(app)
 db = SQLAlchemy(app)
